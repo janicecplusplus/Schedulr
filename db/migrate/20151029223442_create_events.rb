edcd661e2b2aa0e2858ab917217c0747ecd1e347
nisargmodi <nisarg92modi@gmail.com>
2015-10-29 16:15:19 -0700

    fullcalendar gem working

--- db/migrate/20151029223442_create_events.rb
+++ db/migrate/20151029223442_create_events.rb
@@ -1,9 +1,10 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.string :name
+      t.string :title
       t.text :description
-      t.timestamp :time
+      t.datetime :start_time
+      t.datetime :end_time
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029203735) do
+ActiveRecord::Schema.define(version: 20151029223442) do
 
   create_table "events", force: :cascade do |t|
-    t.string   "name"
+    t.string   "title"
     t.text     "description"
-    t.datetime "time"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
