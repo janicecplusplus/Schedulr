79ec3537a3fd62fc651f892a3017fa5900876b8b
Janice See <janiceylsee@ucla.edu>
2015-11-07 18:59:57 -0800

    Redirect to home events page when signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+before_action :check_signed_in
     def landing
+    end
+    def check_signed_in
+    	redirect_to events_path if signed_in?
     end
 end
