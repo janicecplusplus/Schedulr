60e82f381204108e6a94aa27a8db2006e6392e94
enyuhsu <a.new.shoee@gmail.com>
2015-11-13 14:25:04 -0800

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,14 @@ User.create([
   { email: "nisarg@gmail.com", username: "nisarg", password: "lolololol" }
 ])
 
-Friendship.create({ user_id: 1, friend_id: 2, confirmed: 0})
-Friendship.create({ user_id: 1, friend_id: 3, confirmed: 1})
+Friendship.create([
+  { user_id: 1, friend_id: 2, confirmed: 0},
+  { user_id: 1, friend_id: 3, confirmed: 1}
+])
+
+Event.create([
+  { title: "Enyu's Event", start_time: "2015-11-12 18:00:00", end_time: "2015-11-12 19:00:00", user_id: 1},
+  { title: "Janice's Event", start_time: "2015-11-12 18:30:00", end_time: "2015-11-12 19:30:00", user_id: 2},
+  { title: "Tanay's Event", start_time: "2015-11-12 19:00:00", end_time: "2015-11-12 20:00:00", user_id: 3},
+  { title: "Nisarg's Event", start_time: "2015-11-12 19:30:00", end_time: "2015-11-12 20:30:00", user_id: 4}
+])
