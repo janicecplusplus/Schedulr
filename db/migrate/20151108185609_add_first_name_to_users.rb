4b4eef4205c117f49e172ed84247b4cd2a6ac31e
Tanay <tanay992@ucla.edu>
2015-11-28 18:13:34 -0800

    Reduced DB size to 25%

--- db/migrate/20151108185609_add_first_name_to_users.rb
+++ db/migrate/20151108185609_add_first_name_to_users.rb
@@ -1,5 +1,5 @@
 class AddFirstNameToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :lastname, :string
+    add_column :users, :lastname, :string, limit: 20
   end
 end
--- db/migrate/20151109015848_groupify_migration.rb
+++ db/migrate/20151109015848_groupify_migration.rb
@@ -1,9 +1,9 @@
 class GroupifyMigration < ActiveRecord::Migration
   def change
     create_table :groups do |t|
-      t.string     :type
-      t.string :title
-      t.text :description
+      t.string   :type, limit: 10
+      t.string :title, limit: 40
+      t.string :description, limit: 100
     end
 
     create_table :group_memberships do |t|
@@ -11,10 +11,10 @@ class GroupifyMigration < ActiveRecord::Migration
       t.references :group, polymorphic: true ###, index: true
 
       # The named group to which a member belongs (if using)
-      t.string     :group_name ###, index: true
+      t.string     :group_name, limit: 40 ###, index: true
 
       # The membership type the member belongs with
-      t.string     :membership_type
+      #t.string     :membership_type
 
       t.timestamps
     end
