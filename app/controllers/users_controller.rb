3f47a8b419513aca56aadb15a6618dad15f24a0d
Enyu Hsu <a.new.shoee@gmail.com>
2015-12-02 02:40:00 -0800

    fixed pagination bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
   def index
-    @users = User.paginate(:page => params[:page], :per_page => 12)
+    @users = User.paginate(:page => params[:page])
     @user = User.find(current_user.id)
   end
 end
