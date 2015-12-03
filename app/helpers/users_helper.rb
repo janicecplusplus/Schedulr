50c309cc044f4d0f9ab89ccbdfcb63b528750d34
nisargmodi <nisarg92modi@gmail.com>
2015-12-02 19:18:57 -0800

    fragment caching

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+def cache_key_for_user_table
+	"user-table-#{User.maximum(:updated_at)}"
+end
+
+def cache_key_for_user_row(user)
+	"user-#{user.id}-#{user.updated_at}"
+end
+
 end
